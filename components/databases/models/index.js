4ec8d257aae37c48971f28373a64c6ed636d0429
Luis Elizondo <lelizondo@gmail.com>
2014-04-30 00:57:45 -0500

    Reorganized permissions in databases

--- components/databases/models/index.js
+++ components/databases/models/index.js
@@ -1,6 +1,13 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
+/** 
+ * The list of actions that a user can perform in a database are:
+ - view: this includes showing tables, showing users, creating and listing backups
+ - edit: this includes adding users, removing users, locking and unlocking the database
+ - import: includes the import permission
+ - remove: remove the database
+ */
 var Database = mongoose.model("Database", {
 	database_name: String,
 	database_type: String,
@@ -11,7 +18,6 @@ var Database = mongoose.model("Database", {
 		view: [{type: Schema.Types.ObjectId, ref: "User"}],
 		edit: [{type: Schema.Types.ObjectId, ref: "User"}],
 		import: [{type: Schema.Types.ObjectId, ref: "User"}],
-		backup: [{type: Schema.Types.ObjectId, ref: "User"}],
 		remove: [{type: Schema.Types.ObjectId, ref: "User"}],
 	},
 	isLocked: {type: Boolean, default: false},
