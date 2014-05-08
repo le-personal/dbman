e5af76d5ab58eac6751d9da2eef6026edc3d1120
Luis Elizondo <lelizondo@gmail.com>
2014-05-07 23:59:35 -0500

    According to Backbone documentation, when a model does not define a url, it gets the url from the collection

--- public/js/databases/models/databasesuser.js
+++ public/js/databases/models/databasesuser.js
@@ -2,10 +2,6 @@ define(function(require) {
 	var Backbone = require("backbone");
 
 	var DatabasesUser = Backbone.Model.extend({
-		databaseid: null,
-		urlRoot: function() {
-			return "/api/databases/" + this.databaseid + "users";
-		},
 		defaults: {
 			username: '',
 			password: '',
